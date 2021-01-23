8dcc22cf80c68abc6c9b0b4d37967f564bb77057
Artur Balczynski <artur.balczynski@student.put.poznan.pl>
2021-01-23 21:47:55 +0100

    banner ad on intervals activity

--- app/src/main/java/me/sarian/musictheorio/IntervalsActivity.kt
+++ app/src/main/java/me/sarian/musictheorio/IntervalsActivity.kt
@@ -2,10 +2,22 @@ package me.sarian.musictheorio
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
 
 class IntervalsActivity : AppCompatActivity() {
+    private var bannerAdView: AdView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intervals)
+
+        MobileAds.initialize(this) {}
+
+        bannerAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        bannerAdView?.loadAd(adRequest)
+
     }
 }
