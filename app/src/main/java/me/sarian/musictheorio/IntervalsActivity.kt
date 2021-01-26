3f0b28599a7c14ec7da9498dcf58733919ea2c0e
Maciej Stosik <starwars.maciek@gmail.com>
2021-01-26 01:55:37 +0100

    Add interval repeat

--- app/src/main/java/me/sarian/musictheorio/IntervalsActivity.kt
+++ app/src/main/java/me/sarian/musictheorio/IntervalsActivity.kt
@@ -13,6 +13,8 @@ import kotlin.random.Random
 class IntervalsActivity : AppCompatActivity() {
     private var bannerAdView: AdView? = null
     private lateinit var soundPlayer: SoundPlayer
+    private var randomInterval: Int = 0
+    private var randomNote: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,20 +36,6 @@ class IntervalsActivity : AppCompatActivity() {
                 10 to "m7",
                 11 to "M7",
                 12 to "P8"
-        )
-        val notes = mapOf(
-                1 to "C",
-                2 to "C#",
-                3 to "D",
-                4 to "D#",
-                5 to "E",
-                6 to "F",
-                7 to "F#",
-                8 to "G",
-                9 to "G#",
-                10 to "A",
-                11 to "A#",
-                12 to "B"
         )
         val betterNotes = mapOf(
                 1 to "C3",
@@ -80,33 +68,35 @@ class IntervalsActivity : AppCompatActivity() {
         nextButton.setOnClickListener {
             val randInterval = Random.nextInt(1, 12)
             val randRoot = Random.nextInt(1, 12)
-
-//           val rootNote = note.add(intervals[rand])
-//            val rootNote = Note(notes[rand2])
-            val rootNote = notes[randRoot]
+            this.randomInterval = randInterval
+            this.randomNote = randRoot
 
             val textInterval = findViewById<TextView>(R.id.interval)
             val textNote = findViewById<TextView>(R.id.note)
             val textNote2 = findViewById<TextView>(R.id.note2)
 
             textInterval.text = intervals[randInterval]
-            // from
-//            textNote.text = rootNote.name.replace("b", "♭︎ ").replace("#", "♯︎ ")
-//            soundPlayer.playSound("${rootNote.name}3")
-            Timer().schedule(object : TimerTask() {
-                override fun run() {
-                    soundPlayer.playSound(betterNotes[randRoot + randInterval].toString())
-                }
-            }, 200)
+            playInterval(betterNotes, randRoot, randInterval)
             textNote.text = betterNotes[randRoot]!!.dropLast(1)
             textNote2.text = betterNotes[randRoot + randInterval]!!.dropLast(1)
-            soundPlayer.playSound(betterNotes[randRoot].toString())
-//            textNote2.text = rootNote.add(intervals[rand]).name.replace("b", "♭︎ ").replace("#", "♯︎ ")
         }
 
+        val noteView: TextView = findViewById(R.id.note)
+        noteView.setOnClickListener{
+            playInterval(betterNotes, this.randomNote, this.randomInterval)
+        }
 
         bannerAdView = findViewById(R.id.adView)
         val adRequest = AdRequest.Builder().build()
         bannerAdView?.loadAd(adRequest)
+    }
+
+    private fun playInterval(betterNotes: Map<Int, String>, randRoot: Int, randInterval: Int) {
+        soundPlayer.playSound(betterNotes[randRoot].toString())
+        Timer().schedule(object : TimerTask() {
+            override fun run() {
+                soundPlayer.playSound(betterNotes[randRoot + randInterval].toString())
+            }
+        }, 200)
     }
 }
